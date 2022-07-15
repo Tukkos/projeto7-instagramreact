b26e29c716a72608dada54b70546bbf5dd3f8c9a
Tukkos <jpfcastro@id.uff.br>
2022-07-15 11:44:44 -0300

    Posts com o map e funcionando

--- src/App/Content/Column1/PostsColumn/Posts.js
+++ src/App/Content/Column1/PostsColumn/Posts.js
@@ -1,9 +1,31 @@
-export default function Posts() {
+export default function Posts(props) {
+    const post = props.postsInfo;
     return (
         <div>
-            <BarraSuperior />
-            <img src="./Imagens/zane1.jpg" alt="" class="imagem"></img>
-            <BarraInferior />
+            <div class="barra-superior">
+                <div>
+                    <img src={post.userImg} alt="" />
+                    <h2>{post.userName}</h2>
+                </div>
+                <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+            </div>
+
+            <img src={post.postImg} alt="" class="imagem" />
+
+            <div class="barra-inferior">
+                <div class="linha1">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+                <div class="linha2">
+                    <img src={post.likedByImg} alt="" />
+                    <h3>Curtido por <b>{post.likedBy}</b> e <b>{post.likes} pessoas</b></h3>
+                </div>
+            </div>
         </div>
     );
 }
--- src/App/Content/Column1/PostsColumn/PostsColumn.js
+++ src/App/Content/Column1/PostsColumn/PostsColumn.js
@@ -1,51 +1,34 @@
-function BarraInferior(props) {
-    return (
-        <div class="barra-inferior">
-            <div class="linha1">
-                <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-            <div class="linha2">
-                <img src="./Imagens/axton.png" alt="" />
-                <h3>Curtido por &#160</h3>
-                <h2>commando &#160</h2>
-                <h3>e &#160</h3>
-                <h2>254.365 pessoas</h2>
-            </div>
-        </div>
-    );
-}
+import Posts from "./Posts";
 
-function BarraSuperior(props) {
-    return (
-        <div class="barra-superior">
-            <div>
-                <img src="https://www.gamereactor.pt/media/46/borderlands3recension_2964633b.png" alt="" />
-                <h2>notSoTiny</h2>
-            </div>
-            <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
-        </div>
-    );
-}
-
-function Posts(props) {
-    return (
-        <div>
-            <BarraSuperior userImg="https://www.gamereactor.pt/media/46/borderlands3recension_2964633b.png" userName="notSoTiny" />
-            <img src="https://www.gamereactor.pt/media/46/borderlands3recension_2964633b.png" alt="" class="imagem" />
-            <BarraInferior />
-        </div>
-    );
-}
+const posts = [{id: 1,
+                userName: "crazyBoyo",
+                userImg:"https://q46bt2.csb.app/Imagens/zane.png",
+                postImg: "https://q46bt2.csb.app/Imagens/zane1.jpg",
+                likedBy: "commando",
+                likedByImg: "https://q46bt2.csb.app/Imagens/axton.png",
+                likes: 1555423
+                },
+                {id: 2,
+                userName: "notSoTiny",
+                userImg:"https://q46bt2.csb.app/Imagens/tina.png.jpg",
+                postImg: "https://q46bt2.csb.app/Imagens/butt1.jpg",
+                likedBy: "commando",
+                likedByImg: "https://q46bt2.csb.app/Imagens/axton.png",
+                likes: 2998423
+                },
+                {id: 3,
+                userName: "kingTr4p",
+                userImg:"https://q46bt2.csb.app/Imagens/claptrap.png",
+                postImg: "https://q46bt2.csb.app/Imagens/claptrap1.png",
+                likedBy: "commando",
+                likedByImg: "https://q46bt2.csb.app/Imagens/axton.png",
+                likes: 3
+                }]
 
 export default function PostColumn() {
     return (
         <div class="coluna-posts">
-            <Posts postImg="" />
+            {posts.map((post) => (<Posts postsInfo={post} />))}
         </div>
     );
 }

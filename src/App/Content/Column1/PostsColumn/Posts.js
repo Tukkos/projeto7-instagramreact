1ca5018fa97500141c1b484f133eb360db9d37c1
Tukkos <jpfcastro@id.uff.br>
2022-07-15 08:22:29 -0300

    teste 2

--- src/App/Content/Column1/PostsColumn/Posts.js
+++
@@ -1,9 +0,0 @@
-export default function Posts() {
-    return (
-        <div>
-            <BarraSuperior />
-            <img src="./Imagens/zane1.jpg" alt="" class="imagem"></img>
-            <BarraInferior />
-        </div>
-    );
-}
--- src/App/TopBar/TopBarMobile.js
+++
@@ -1,9 +0,0 @@
-export default function TopBarMobile() {
-    return (
-        <div class="barra-fundo-mobile">
-        <ion-icon name="logo-instagram"></ion-icon>
-        <img src="./Imagens/logo.png" alt="" />
-        <ion-icon name="paper-plane-outline"></ion-icon>
-    </div>
-    );
-}
